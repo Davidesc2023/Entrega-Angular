fd5059290b30b247de26162e9bd287006ea2f282
David Sanguino <david.sang197@gmail.com>
2024-10-14 12:43:28 -0500

    Añadir proyecto Angular para la panadería Pancolombia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },  // Ruta principal
+  { path: 'productos', component: ProductosComponent },  // Ruta para productos
+  { path: 'contacto', component: ContactoComponent },  // Ruta para contacto
+];
